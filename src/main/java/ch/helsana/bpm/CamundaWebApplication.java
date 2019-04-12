609cfad1741dc54e7c6e4e726035f9eaa88f7c5b
Michael Würsch <wuersch@me.com>
2019-04-12 22:59:24 +0200

    added zul reverse proxy to link forms

--- src/main/java/ch/helsana/bpm/CamundaWebApplication.java
+++ src/main/java/ch/helsana/bpm/CamundaWebApplication.java
@@ -2,8 +2,10 @@ package ch.helsana.bpm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class CamundaWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(CamundaWebApplication.class, args);
